39fb4b2f506086b73eecb878db35eab404f0ed89
Njit-Tao7 <tao7@njit.edu>
2022-12-01 20:12:30 -0500

    header stuff with logo

--- web/src/pages/LandingPage/LandingPage.js
+++ web/src/pages/LandingPage/LandingPage.js
@@ -21,6 +21,10 @@ const LandingPage = () => {
         <div className="navigation">
           <nav>
             <ul>
+
+              <li>
+                <i className="bi bi-calendar-check-fill"></i>
+              </li>
               <li>
                 <Link to={routes.landingPage()}>Home</Link>
               </li>
@@ -48,6 +52,18 @@ const LandingPage = () => {
                     <Button style={{ color: 'black' }}>Log In 🖋</Button>
                   </Link>
                 )}
+              </li>
+              <li>
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  width="50"
+                  height="50"
+                  fill="currentColor"
+                  className="bi bi-calendar-check-fill"
+                  viewBox="0 0 16 16"
+                >
+                  <path d="M4 .5a.5.5 0 0 0-1 0V1H2a2 2 0 0 0-2 2v1h16V3a2 2 0 0 0-2-2h-1V.5a.5.5 0 0 0-1 0V1H4V.5zM16 14V5H0v9a2 2 0 0 0 2 2h12a2 2 0 0 0 2-2zm-5.146-5.146-3 3a.5.5 0 0 1-.708 0l-1.5-1.5a.5.5 0 0 1 .708-.708L7.5 10.793l2.646-2.647a.5.5 0 0 1 .708.708z" />
+                </svg>
               </li>
               {currentUser ? (
                 <li
